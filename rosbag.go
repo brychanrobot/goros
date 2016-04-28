d1d213adbbb2f27ae5a0151a6a463e61a9ec52c7
Bryant Chandler <brychanrobot@gmail.com>
2016-04-27 22:32:43 -0600

    fixed buffer size problems so parsing a whole bag works

--- rosbag.go
+++ rosbag.go
@@ -4,14 +4,25 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/binary"
+	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
+const (
+	MessageData      = 0x02
+	BagHeader        = 0x03
+	IndexData        = 0x04
+	ChunkRecord      = 0x05
+	ChunkInfo        = 0x06
+	ConnectionRecord = 0x07
+)
+
 func parseRecordHeader(buffer []byte) map[string]interface{} {
-	log.Println(string(buffer))
+	//log.Println(string(buffer))
 	reader := bufio.NewReader(bytes.NewReader(buffer))
-	log.Println(reader.Buffered())
+	//log.Println(reader.Buffered())
 
 	valueMap := make(map[string]interface{})
 
@@ -25,24 +36,20 @@ func parseRecordHeader(buffer []byte) map[string]interface{} {
 
 		switch fieldName {
 		case "op":
-			opCode, _ := reader.ReadByte()
-			valueMap[fieldName] = opCode
-			//log.Printf("%s%d", fieldName, opCode)
-		case "compression":
+			value, _ := reader.ReadByte()
+			valueMap[fieldName] = value
+		case "conn", "size", "conn_count", "chunk_count", "count", "ver":
+			var value uint32
+			binary.Read(reader, binary.LittleEndian, &value)
+			valueMap[fieldName] = value
+		case "index_pos", "time", "start_time", "end_time", "chunk_pos":
+			var value uint64
+			binary.Read(reader, binary.LittleEndian, &value)
+			valueMap[fieldName] = value
+		case "compression", "topic":
 			value := make([]byte, valueLength)
 			reader.Read(value)
 			valueMap[fieldName] = string(value)
-			//log.Printf("%s%s", fieldName, string(value))
-		case "conn":
-			var connectionId uint32
-			binary.Read(reader, binary.LittleEndian, &connectionId)
-			valueMap[fieldName] = connectionId
-			//log.Printf("%s%d", fieldName, connectionId)
-		case "size":
-			var uncompressedSize uint32
-			binary.Read(reader, binary.LittleEndian, &uncompressedSize)
-			valueMap[fieldName] = uncompressedSize
-			//log.Printf("%s%d", fieldName, uncompressedSize)
 		default:
 			value := make([]byte, valueLength)
 			reader.Read(value)
@@ -54,30 +61,54 @@ func parseRecordHeader(buffer []byte) map[string]interface{} {
 	return valueMap
 }
 
-func parseRecord(reader *bufio.Reader, skip bool) {
+func parseRecord(reader *bufio.Reader) error {
 	var headerLength uint32
-	binary.Read(reader, binary.LittleEndian, &headerLength)
-	log.Println(headerLength)
-	if skip {
-		reader.Discard(int(headerLength))
-	} else {
-		buffer := make([]byte, int(headerLength))
-		reader.Read(buffer)
-		valueMap := parseRecordHeader(buffer)
-		log.Println(valueMap)
-		//log.Println(string(buffer))
+	err := binary.Read(reader, binary.LittleEndian, &headerLength)
+	if err != nil {
+		return err
 	}
+	//log.Println(headerLength)
+	buffer := make([]byte, int(headerLength))
+	reader.Read(buffer)
+	valueMap := parseRecordHeader(buffer)
+	log.Println(valueMap)
 
 	var dataLength uint32
 	binary.Read(reader, binary.LittleEndian, &dataLength)
-	log.Println(dataLength)
-	if skip {
-		reader.Discard(int(dataLength))
-	} else {
+	//log.Println(dataLength)
+	//if skip {
+	//reader.Discard(int(dataLength))
+	dataBuffer := make([]byte, int(dataLength))
+	bytesRead, _ := io.ReadFull(reader, dataBuffer)
+	//log.Println(bytesRead)
+
+	if bytesRead != int(dataLength) {
+		panic(fmt.Sprintf("expected %d bytes, got %d", dataLength, bytesRead))
+	}
+	/*} else {
 		buffer := make([]byte, int(dataLength))
 		reader.Read(buffer)
-		log.Println(string(buffer[:5000]))
+		log.Println(string(buffer))
 	}
+	*/
+	switch valueMap["op"].(byte) {
+	case BagHeader:
+	case ChunkRecord:
+		chunkReader := bufio.NewReaderSize(bytes.NewReader(dataBuffer), 8000000)
+		var chunkErr error
+		for chunkErr == nil {
+			chunkErr = parseRecord(chunkReader)
+		}
+	case ConnectionRecord:
+		//log.Println(string(dataBuffer))
+		dataMap := parseRecordHeader(dataBuffer)
+		log.Println(dataMap)
+	case MessageData:
+	case IndexData:
+	case ChunkInfo:
+	}
+
+	return nil
 
 }
 
@@ -89,13 +120,17 @@ func ParseRosbag(path string) {
 	}
 	defer file.Close()
 
-	reader := bufio.NewReader(file)
+	reader := bufio.NewReaderSize(file, 8000000)
 
 	line, _ := reader.ReadString('\n')
 	log.Print(line)
 
-	parseRecord(reader, true)
-	parseRecord(reader, false)
+	//parseRecord(reader)
+	//parseRecord(reader)
+	var recordError error
+	for recordError == nil {
+		recordError = parseRecord(reader)
+	}
 	/*chunkHeader := make([]byte, 4096)
 	_, _ = file.Read(chunkHeader)
 	log.Println(string(buffer))
